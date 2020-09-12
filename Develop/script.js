3f62e3abf5a3793f8b42354fddb1b71ef4792b53
Dylan Nguyen <babydylannguyen@gmail.com>
2020-09-11 17:58:17 -0700

    completely working generator with upper lower numbers and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,40 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+var strLower = "abcdefghijklmnopqrstuvwxyz";
+var strUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var strNums = "0123456789";
+var strSpecial = "!\"\\#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var result = "";
+  var random = "";
+  var numChars = prompt("How long do you want your password? (8-128 characters)");
+  var lower = confirm("Do you want lowercase letters?");
+  var upper = confirm("Do you want uppercase letters?");
+  var nums = confirm("Do you want numbers?");
+  var special = confirm("Do you want special characters?");
+  if (numChars < 8 || numChars > 128) {
+    alert("Password length must be from 8 to 128.");
+  }
+  if (lower === true) {
+    random = random + strLower;
+  }
+  if (upper === true) {
+    random = random + strUpper;
+  }
+  if (nums === true) {
+    random = random + strNums;
+  }
+  if (special === true) {
+    random = random + strSpecial;
+  }
+
+
+     for (var i = 0; i < numChars; i++) {
+       result = result + random.charAt(Math.floor(Math.random() * (random.length - 1)));
+     }
+
+    return result;
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # password-generator
+
+## Description 
+
+Password generator where you can choose to add lowercase, uppercase, numbers, and symbols.
+
+https://babydylan.github.io/password-generator/
+## Table of Contents (Optional)
+
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+Goto the webpage link.
+
+## Usage 
+
+Goto webpage link. Click "Generate Password" button. Enter the length you want and confirm whether you want lowercase, uppercase, numbers, and/or special characters. Then it generates a password.
+
+
+© 2020 Baby Dylan Nguyen
